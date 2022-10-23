9610926568eeb1d20f6618126fe7e76dfb0edf32
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-10-23 16:04:17 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function  Sidebar() {
@@ -9,11 +11,17 @@ export function  Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/osvaldokalvaitir.png"
+        />
+
         <strong>Osvaldo Kalvaitir</strong>
-        <span>Web Developer</span>
+        <span>FullStack Developer</span>
 
         <footer>
           <a href="#">
+            <PencilSimpleLine size={20} />
             Editar seu perfil
           </a>
         </footer>

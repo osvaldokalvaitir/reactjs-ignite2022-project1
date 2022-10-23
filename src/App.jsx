b9198fbd4f50f75d7e775821d545970ac0e08094
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-10-23 15:27:23 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
 import { Post } from './Post';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -8,15 +12,21 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Osvaldo Kalvaitir"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita necessitatibus quaerat vero sit magni, veniam mollitia accusantium beatae illo quam eius voluptates laborum dicta? Ducimus laborum nihil pariatur recusandae quas."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        author="Alessandra Kalvaitir"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur modi sequi qui. Similique sapiente ab incidunt vel blanditiis neque unde. Iusto totam laborum porro vel qui et dolorum libero a."
-      />
+        <main>
+          <Post
+            author="Osvaldo Kalvaitir"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita necessitatibus quaerat vero sit magni, veniam mollitia accusantium beatae illo quam eius voluptates laborum dicta? Ducimus laborum nihil pariatur recusandae quas."
+          />
+
+          <Post
+            author="Alessandra Kalvaitir"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur modi sequi qui. Similique sapiente ab incidunt vel blanditiis neque unde. Iusto totam laborum porro vel qui et dolorum libero a."
+          />
+        </main>
+      </div>
     </div>
   )
 }

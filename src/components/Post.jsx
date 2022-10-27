82772d850d5970d339391254011400a06ddca2c3
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-10-26 21:49:11 -0300

    Componente: Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
@@ -7,10 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/osvaldokalvaitir.png"
-          />
+          <Avatar src="https://github.com/osvaldokalvaitir.png" />
 
           <div className={styles.authorInfo}>
             <strong>Osvaldo Kalvaitir</strong>
